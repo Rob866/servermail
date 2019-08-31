5f1732db7589d46b0ccf7e833ce87617510b7111
robert <juanrob_10@hotmail.com>
2019-08-31 16:03:48 -0700

    first

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const nodemailer = require('nodemailer');
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/send', function(req,res){
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+      user: 'juanrro10@gmail.com',
+      pass: '19198663151'    
+    }    
+  })
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: 'juanrro10@gmail.com',
+    subject: `${req.body.nombre}`,
+    text: `Nombre: ${req.body.nombre}.Teléfono: ${req.body.telefono}.Mensaje: ${req.body.mensaje}`,
+    replyTo: `${req.body.email}`
+  }
+  transporter.sendMail(mailOptions, function(err, resp) {
+    if (err) {
+      console.error('A  ocurrido un error: ', err);
+    } else {
+        res.send(req.body)
+    }
+  })
+  
+
+})
+
+
+app.listen(3000, function(){
+  console.log('servidor corriendo en el puerto 3000')    
+})
+
+
+     
